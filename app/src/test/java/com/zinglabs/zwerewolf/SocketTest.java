dd0188cab6c9557ddcb63493063df5e0331fc14e
MomentQYC <62551256+MomentQYC@users.noreply.github.com>
2022-06-30 15:26:23 +0800

    Update SocketTest.java

--- app/src/test/java/com/zinglabs/zwerewolf/SocketTest.java
+++ app/src/test/java/com/zinglabs/zwerewolf/SocketTest.java
@@ -13,7 +13,7 @@ public class SocketTest {
 
 
     public static void main(String[] args) throws Exception{
-        Socket client = new Socket("183k141i84.imwork.net", 31260);
+        Socket client = new Socket("ww.yatserver.com", 3126);
         client.setSoTimeout(5000);
         PrintStream out = new PrintStream(client.getOutputStream());
         out.print("hello");
